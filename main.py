b79e37534a789579f8a3cee88ade721ad46d6111
Harry <harry.b21@gmail.com>
2021-12-11 10:11:47 +0200

    Part two in Tic Tac Toe Added function to create the game board on the pygame screen. Added function to create the board represented in a list. added a function to create a list of pygameRect objects to detect mouse collision detection.

--- main.py
+++ main.py
@@ -6,14 +6,55 @@ pygame.init()
 WIDTH = 900
 HEIGHT = 900
 
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 #  Creating the pygame screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic Tac Toe")
 
 
+def board_rects(board):
+    """Creates an array of pygame Rectangles in order to enable Mouse Collision Detection"""
+    boardRect = []
+    for a, rows in enumerate(board):
+        columnList = []
+        for b, columns in enumerate(rows):
+            columnList.append(pygame.Rect(WIDTH//3*b, HEIGHT//3*a, WIDTH//3, HEIGHT//3))
+        boardRect.append(columnList)
+    return boardRect
+
+
+def board():
+    """This function creates the game board in an array"""
+    board = []
+    for i in range(1, 4):
+        columnList = []
+        for x in range(1, 4):
+            columnList.append(0)
+        board.append(columnList)
+    return board
+
+
+def draw_board():
+    """This function draws the game background to the screen"""
+    screen.fill(WHITE)
+    for i in range(1, 3):
+        pygame.draw.line(screen, BLACK, (WIDTH//3 * i, 25), (WIDTH//3 * i, HEIGHT - 25), 10)
+        pygame.draw.line(screen, BLACK, (25, HEIGHT//3 * i), (WIDTH-25, HEIGHT//3 * i), 10)
+
+
+board = board()
+boardRect = board_rects(board)
+
 gameLoop = True
 while gameLoop:
+
+    # Draw the board to the screen
+    draw_board()
 
     #  Event handler
     for event in pygame.event.get():
